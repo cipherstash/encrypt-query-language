8b9b26eece09e35f1aba958658b4933480d41ba3
CJ Brewer <brewercalvinj@gmail.com>
2024-10-02 18:33:07 -0600

    chore(js): lint

--- javascript/apps/drizzle/src/insert.ts
+++ javascript/apps/drizzle/src/insert.ts
@@ -1,39 +1,39 @@
-import { parseArgs } from "node:util";
-import { eqlPayload } from "@cipherstash/eql";
-import { users } from "./schema";
-import { db } from "./db";
+import { parseArgs } from 'node:util'
+import { eqlPayload } from '@cipherstash/eql'
+import { db } from './db'
+import { users } from './schema'
 
 const { values, positionals } = parseArgs({
-	args: Bun.argv,
-	options: {
-		email: {
-			type: "string",
-		},
-	},
-	strict: true,
-	allowPositionals: true,
-});
+  args: Bun.argv,
+  options: {
+    email: {
+      type: 'string',
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+})
 
-const email = values.email;
+const email = values.email
 
 if (!email) {
-	throw new Error("[ERROR] the email command line argument is required");
+  throw new Error('[ERROR] the email command line argument is required')
 }
 
 await db
-	.insert(users)
-	.values({
-		email,
-		email_encrypted: eqlPayload({
-			plaintext: email,
-			table: "users",
-			column: "email_encrypted",
-		}),
-	})
-	.execute();
+  .insert(users)
+  .values({
+    email,
+    email_encrypted: eqlPayload({
+      plaintext: email,
+      table: 'users',
+      column: 'email_encrypted',
+    }),
+  })
+  .execute()
 
 console.log(
-	"[INFO] You've inserted a new user with an encrypted email from the plaintext",
-	email,
-);
-process.exit(0);
+  "[INFO] You've inserted a new user with an encrypted email from the plaintext",
+  email,
+)
+process.exit(0)
--- javascript/apps/drizzle/src/select.ts
+++ javascript/apps/drizzle/src/select.ts
@@ -1,24 +1,24 @@
-import { getPlaintext } from "@cipherstash/eql";
-import { users } from "./schema";
-import { db } from "./db";
+import { getPlaintext } from '@cipherstash/eql'
+import { db } from './db'
+import { users } from './schema'
 
 const sqlResult = db
-	.select({
-		email: users.email_encrypted,
-	})
-	.from(users)
-	.toSQL();
+  .select({
+    email: users.email_encrypted,
+  })
+  .from(users)
+  .toSQL()
 
-const data = await db.select().from(users).execute();
+const data = await db.select().from(users).execute()
 
-console.log("[INFO] SQL statement:", sqlResult.sql);
-console.log("[INFO] All emails have been decrypted by CipherStash Proxy");
+console.log('[INFO] SQL statement:', sqlResult.sql)
+console.log('[INFO] All emails have been decrypted by CipherStash Proxy')
 console.log(
-	"Emails:",
-	JSON.stringify(
-		data.map((row) => getPlaintext(row.email_encrypted)),
-		null,
-		2,
-	),
-);
-process.exit(0);
+  'Emails:',
+  JSON.stringify(
+    data.map((row) => getPlaintext(row.email_encrypted)),
+    null,
+    2,
+  ),
+)
+process.exit(0)
