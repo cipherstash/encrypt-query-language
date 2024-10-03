17b138ee8ba739301b16c4a2dcb05666686bd9df
CJ Brewer <brewercalvinj@gmail.com>
2024-10-02 18:33:07 -0600

    chore(js): lint

--- javascript/packages/eql/src/index.ts
+++ javascript/packages/eql/src/index.ts
@@ -1,46 +1,46 @@
-export * from "./cs_encrypted_v1";
-import type { CsEncryptedV1Schema } from "./cs_encrypted_v1";
+export * from './cs_encrypted_v1'
+import type { CsEncryptedV1Schema } from './cs_encrypted_v1'
 
 type EqlPayload = {
-	plaintext: string;
-	table: string;
-	column: string;
-	version?: number;
-	schemaVersion?: number;
-};
+  plaintext: string
+  table: string
+  column: string
+  version?: number
+  schemaVersion?: number
+}
 
 export const eqlPayload = ({
-	plaintext,
-	table,
-	column,
-	version = 1,
+  plaintext,
+  table,
+  column,
+  version = 1,
 }: EqlPayload): CsEncryptedV1Schema => {
-	return {
-		v: version,
-		s: 1,
-		k: "pt",
-		p: plaintext,
-		i: {
-			t: table,
-			c: column,
-		},
-	};
-};
+  return {
+    v: version,
+    s: 1,
+    k: 'pt',
+    p: plaintext,
+    i: {
+      t: table,
+      c: column,
+    },
+  }
+}
 
 export const getPlaintext = (
-	payload: CsEncryptedV1Schema | null,
+  payload: CsEncryptedV1Schema | null,
 ): string | undefined => {
-	if (payload?.k === "pt") {
-		return payload.p;
-	}
-	return undefined;
-};
+  if (payload?.k === 'pt') {
+    return payload.p
+  }
+  return undefined
+}
 
 export const getCiphertext = (
-	payload: CsEncryptedV1Schema | null,
+  payload: CsEncryptedV1Schema | null,
 ): string | undefined => {
-	if (payload?.k === "ct") {
-		return payload.c;
-	}
-	return undefined;
-};
+  if (payload?.k === 'ct') {
+    return payload.c
+  }
+  return undefined
+}
