95c981e728411897469d8b11ef5f5805e2e24801
CJ Brewer <brewercalvinj@gmail.com>
2024-10-10 12:56:52 -0600

    feat(content): update readme

--- cipherstash/start.sh
+++ cipherstash/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -p 6432:6432 -e CS_STATEMENT_HANDLER=mylittleproxy -e LOG_LEVEL=debug -v $(pwd)/cipherstash-proxy.toml:/etc/cipherstash-proxy/cipherstash-proxy.toml cipherstash/cipherstash-proxy:cipherstash-proxy-v0.0.25
+# The version is hard coded as we are in active development of some of the EQL features
+docker run -p 6432:6432 -e CS_STATEMENT_HANDLER=mylittleproxy -e LOG_LEVEL=debug -v $(pwd)/cipherstash-proxy.toml:/etc/cipherstash-proxy/cipherstash-proxy.toml cipherstash/cipherstash-proxy:pr-1008
