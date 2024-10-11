71c64e87b96a9845af7a6eb3c465a01607ff8824
Yuji Yokoo <yujiyokoo@gmail.com>
2024-10-11 17:44:32 +1030

    Update index creation, add package install step

--- python/jupyter_notebook/create_examples_table.sql
+++ python/jupyter_notebook/create_examples_table.sql
@@ -6,8 +6,17 @@ create table examples (
   encrypted_float examples__encrypted_float,
   encrypted_int examples__encrypted_int,
   encrypted_small_int examples__encrypted_small_int,
-  encrypted_utf8_str examples__encrypted_utf8_str
+  encrypted_utf8_str examples__encrypted_utf8_str,
+  encrypted_jsonb examples__encrypted_jsonb
 );
 
-CREATE UNIQUE INDEX encrypted_utf8_str_unique_index
-	ON examples( (encrypted_utf8_str->>'u') );
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_big_int));
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_boolean));
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_date));
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_float));
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_int));
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_small_int));
+CREATE UNIQUE INDEX ON examples(cs_unique_v1(encrypted_utf8_str));
+CREATE INDEX ON examples USING GIN (cs_match_v1(encrypted_utf8_str));
+CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_utf8_str));
+CREATE INDEX ON examples USING GIN (cs_ste_vec_v1(encrypted_jsonb));
