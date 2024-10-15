b508dfc42bdbdf4e0be983fd4d1669e773de7028
fimac <fiona@cipherstash.com>
2024-10-15 15:13:44 +1100

    create separate package for common function to serialize

--- go/xorm/migrations.go
+++ go/xorm/migrations.go
@@ -364,17 +364,17 @@ func installDsl(engine *sql.DB) {
 // If this is not the case, then we will receive a postgres constraint violation.
 func AddConstraint(engine *sql.DB) {
 	sql := `
-	ALTER TABLE examples ADD CONSTRAINT encrypted_text_encrypted_check
-	CHECK ( cs_check_encrypted_v1(encrypted_text) );
+	ALTER TABLE examples ADD CONSTRAINT encrypted_text_field_encrypted_check
+	CHECK ( cs_check_encrypted_v1(encrypted_text_field) );
 
 	ALTER TABLE examples ADD CONSTRAINT encrypted_jsonb_encrypted_check
-	CHECK ( cs_check_encrypted_v1(encrypted_jsonb) );
+	CHECK ( cs_check_encrypted_v1(encrypted_jsonb_field) );
 
 	ALTER TABLE examples ADD CONSTRAINT encrypted_int_encrypted_check
-	CHECK ( cs_check_encrypted_v1(encrypted_int) );
+	CHECK ( cs_check_encrypted_v1(encrypted_int_field) );
 
-	ALTER TABLE examples ADD CONSTRAINT encrypted_bool_encrypted_check
-	CHECK ( cs_check_encrypted_v1(encrypted_bool) );
+	-- ALTER TABLE examples ADD CONSTRAINT encrypted_bool_encrypted_check
+	-- CHECK ( cs_check_encrypted_v1(encrypted_bool_field) );
 	`
 
 	_, err := engine.Exec(sql)
@@ -389,19 +389,19 @@ func AddConstraint(engine *sql.DB) {
 // This configuration is needed to determine how the data is encrypted and how you can query
 func AddIndexes(engine *sql.DB) {
 	sql := `
-	  SELECT cs_add_index_v1('examples', 'encrypted_text', 'unique', 'text', '{"token_filters": [{"kind": "downcase"}]}');
-      SELECT cs_add_index_v1('examples', 'encrypted_text', 'match', 'text');
-      SELECT cs_add_index_v1('examples', 'encrypted_text', 'ore', 'text');
-      SELECT cs_add_index_v1('examples', 'encrypted_int', 'ore', 'int');
-      SELECT cs_add_index_v1('examples', 'encrypted_bool', 'ore', 'boolean');
-	  SELECT cs_add_index_v1('examples', 'encrypted_jsonb', 'ste_vec', 'jsonb', '{"prefix": "some-prefix"}');
+	  SELECT cs_add_index_v1('examples', 'encrypted_text_field', 'unique', 'text', '{"token_filters": [{"kind": "downcase"}]}');
+      SELECT cs_add_index_v1('examples', 'encrypted_text_field', 'match', 'text');
+      SELECT cs_add_index_v1('examples', 'encrypted_text_field', 'ore', 'text');
+      SELECT cs_add_index_v1('examples', 'encrypted_int_field', 'ore', 'int');
+	  SELECT cs_add_index_v1('examples', 'encrypted_jsonb_field', 'ste_vec', 'jsonb', '{"prefix": "some-prefix"}');
+      SELECT cs_add_index_v1('examples', 'encrypted_bool_field', 'ore', 'boolean');
 
-	  CREATE UNIQUE INDEX ON examples(cs_unique_v1(encrypted_text));
-      CREATE INDEX ON examples USING GIN (cs_match_v1(encrypted_text));
-      CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_text));
-      CREATE INDEX ON examples USING GIN (cs_ste_vec_v1(encrypted_jsonb));
-	  CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_int));
-	  CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_bool));
+	  CREATE UNIQUE INDEX ON examples(cs_unique_v1(encrypted_text_field));
+      CREATE INDEX ON examples USING GIN (cs_match_v1(encrypted_text_field));
+      CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_text_field));
+      CREATE INDEX ON examples USING GIN (cs_ste_vec_v1(encrypted_jsonb_field));
+	  CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_int_field));
+	  CREATE INDEX ON examples (cs_ore_64_8_v1(encrypted_bool_field));
 
       SELECT cs_encrypt_v1();
       SELECT cs_activate_v1();
