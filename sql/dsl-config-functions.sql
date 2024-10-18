b5d479f3413a624954f805943c08a8e7a195f3d7
fimac <fiona@cipherstash.com>
2024-10-18 15:11:11 +1100

    move over dsl sql

---
+++ sql/dsl-config-functions.sql
@@ -0,0 +1,376 @@
+--
+-- Configuration functions
+--
+--
+
+
+-- DROP and CREATE functions
+-- Function types cannot be changed after creation so we DROP for flexibility
+
+DROP FUNCTION IF EXISTS cs_add_column_v1(text, text);
+DROP FUNCTION IF EXISTS cs_remove_column_v1(text, text);
+DROP FUNCTION IF EXISTS cs_add_index_v1(text, text, text, jsonb);
+DROP FUNCTION IF EXISTS cs_remove_index_v1(text, text, text);
+DROP FUNCTION IF EXISTS cs_modify_index_v1(text, text, text, jsonb);
+
+DROP FUNCTION IF EXISTS cs_encrypt_v1();
+DROP FUNCTION IF EXISTS cs_activate_v1();
+DROP FUNCTION IF EXISTS cs_discard_v1();
+
+DROP FUNCTION IF EXISTS cs_refresh_encrypt_config();
+
+DROP FUNCTION IF EXISTS _cs_config_default();
+DROP FUNCTION IF EXISTS _cs_config_match_default();
+
+DROP FUNCTION IF EXISTS _cs_config_add_table(text, json);
+DROP FUNCTION IF EXISTS _cs_config_add_column(text, text, json);
+DROP FUNCTION IF EXISTS _cs_config_add_cast(text, text, text, json);
+DROP FUNCTION IF EXISTS _cs_config_add_index(text, text, text, json, json);
+
+
+CREATE FUNCTION _cs_config_default(config jsonb)
+  RETURNS jsonb
+  IMMUTABLE PARALLEL SAFE
+AS $$
+  BEGIN
+    IF config IS NULL THEN
+      SELECT jsonb_build_object('v', 1, 'tables', jsonb_build_object()) INTO config;
+    END IF;
+    RETURN config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION _cs_config_add_table(table_name text, config jsonb)
+  RETURNS jsonb
+  -- IMMUTABLE PARALLEL SAFE
+AS $$
+  DECLARE
+    tbl jsonb;
+  BEGIN
+    IF NOT config #> array['tables'] ? table_name THEN
+      SELECT jsonb_insert(config, array['tables', table_name], jsonb_build_object()) INTO config;
+    END IF;
+    RETURN config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+-- Add the column if it doesn't exist
+CREATE FUNCTION _cs_config_add_column(table_name text, column_name text, config jsonb)
+  RETURNS jsonb
+  IMMUTABLE PARALLEL SAFE
+AS $$
+  DECLARE
+    col jsonb;
+  BEGIN
+    IF NOT config #> array['tables', table_name] ? column_name THEN
+      SELECT jsonb_build_object('indexes', jsonb_build_object()) into col;
+      SELECT jsonb_set(config, array['tables', table_name, column_name], col) INTO config;
+    END IF;
+    RETURN config;
+  END;
+$$ LANGUAGE plpgsql;
+
+-- Set the cast
+CREATE FUNCTION _cs_config_add_cast(table_name text, column_name text, cast_as text, config jsonb)
+  RETURNS jsonb
+  IMMUTABLE PARALLEL SAFE
+AS $$
+  BEGIN
+    SELECT jsonb_set(config, array['tables', table_name, column_name, 'cast_as'], to_jsonb(cast_as)) INTO config;
+    RETURN config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+-- Add the column if it doesn't exist
+CREATE FUNCTION _cs_config_add_index(table_name text, column_name text, index_name text, opts jsonb, config jsonb)
+  RETURNS jsonb
+  IMMUTABLE PARALLEL SAFE
+AS $$
+  BEGIN
+    SELECT jsonb_insert(config, array['tables', table_name, column_name, 'indexes', index_name], opts) INTO config;
+    RETURN config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+--
+-- Default options for match index
+--
+CREATE FUNCTION _cs_config_match_default()
+  RETURNS jsonb
+LANGUAGE sql STRICT PARALLEL SAFE
+BEGIN ATOMIC
+  SELECT jsonb_build_object(
+            'k', 6,
+            'm', 2048,
+            'include_original', true,
+            'tokenizer', json_build_object('kind', 'ngram', 'token_length', 3),
+            'token_filters', json_build_array(json_build_object('kind', 'downcase')));
+END;
+
+--
+--
+--
+CREATE FUNCTION cs_add_index_v1(table_name text, column_name text, index_name text, cast_as text DEFAULT 'text', opts jsonb DEFAULT '{}')
+  RETURNS jsonb
+AS $$
+  DECLARE
+    o jsonb;
+    _config jsonb;
+  BEGIN
+
+    -- set the active config
+    SELECT data INTO _config FROM cs_configuration_v1 WHERE state = 'active' OR state = 'pending' ORDER BY state DESC;
+
+    -- if index exists
+    IF _config #> array['tables', table_name, column_name, 'indexes'] ?  index_name THEN
+      RAISE EXCEPTION '% index exists for column: % %', index_name, table_name, column_name;
+    END IF;
+
+    IF NOT cast_as = ANY('{text, int, small_int, big_int, real, double, boolean, date, jsonb}') THEN
+      RAISE EXCEPTION '% is not a valid cast type', cast_as;
+    END IF;
+
+    -- set default config
+    SELECT _cs_config_default(_config) INTO _config;
+
+    SELECT _cs_config_add_table(table_name, _config) INTO _config;
+
+    SELECT _cs_config_add_column(table_name, column_name, _config) INTO _config;
+
+    SELECT _cs_config_add_cast(table_name, column_name, cast_as, _config) INTO _config;
+
+    -- set default options for index if opts empty
+    IF index_name = 'match' AND opts = '{}' THEN
+      SELECT _cs_config_match_default() INTO opts;
+    END IF;
+
+    SELECT _cs_config_add_index(table_name, column_name, index_name, opts, _config) INTO _config;
+
+    --  create a new pending record if we don't have one
+    INSERT INTO cs_configuration_v1 (state, data) VALUES ('pending', _config)
+    ON CONFLICT (state)
+      WHERE state = 'pending'
+    DO UPDATE
+      SET data = _config;
+
+    -- exeunt
+    RETURN _config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION cs_remove_index_v1(table_name text, column_name text, index_name text)
+  RETURNS jsonb
+AS $$
+  DECLARE
+    _config jsonb;
+  BEGIN
+
+    -- set the active config
+    SELECT data INTO _config FROM cs_configuration_v1 WHERE state = 'active' OR state = 'pending' ORDER BY state DESC;
+
+    -- if no config
+    IF _config IS NULL THEN
+      RAISE EXCEPTION 'No active or pending configuration exists';
+    END IF;
+
+    -- if the table doesn't exist
+    IF NOT _config #> array['tables'] ? table_name THEN
+      RAISE EXCEPTION 'No configuration exists for table: %', table_name;
+    END IF;
+
+    -- if the index does not exist
+    -- IF NOT _config->key ? index_name THEN
+    IF NOT _config #> array['tables', table_name] ?  column_name THEN
+      RAISE EXCEPTION 'No % index exists for column: % %', index_name, table_name, column_name;
+    END IF;
+
+    --  create a new pending record if we don't have one
+    INSERT INTO cs_configuration_v1 (state, data) VALUES ('pending', _config)
+    ON CONFLICT (state)
+      WHERE state = 'pending'
+    DO NOTHING;
+
+    -- remove the index
+    SELECT _config #- array['tables', table_name, column_name, 'indexes', index_name] INTO _config;
+
+    -- if column is now empty, remove the column
+    IF _config #> array['tables', table_name, column_name, 'indexes'] = '{}' THEN
+      SELECT _config #- array['tables', table_name, column_name] INTO _config;
+    END IF;
+
+    -- if table  is now empty, remove the table
+    IF _config #> array['tables', table_name] = '{}' THEN
+      SELECT _config #- array['tables', table_name] INTO _config;
+    END IF;
+
+    -- if config empty delete
+    -- or update the config
+    IF _config #> array['tables'] = '{}' THEN
+      DELETE FROM cs_configuration_v1 WHERE state = 'pending';
+    ELSE
+      UPDATE cs_configuration_v1 SET data = _config WHERE state = 'pending';
+    END IF;
+
+    -- exeunt
+    RETURN _config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION cs_modify_index_v1(table_name text, column_name text, index_name text, cast_as text DEFAULT 'text', opts jsonb DEFAULT '{}')
+  RETURNS jsonb
+AS $$
+  BEGIN
+    PERFORM cs_remove_index_v1(table_name, column_name, index_name);
+    RETURN cs_add_index_v1(table_name, column_name, index_name, cast_as, opts);
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION cs_encrypt_v1()
+  RETURNS boolean
+AS $$
+	BEGIN
+    -- IF NOT cs_ready_for_encryption_v1() THEN
+    --   RAISE EXCEPTION 'Some pending columns do not have an encrypted target';
+    -- END IF;
+
+		IF NOT EXISTS (SELECT FROM cs_configuration_v1 c WHERE c.state = 'pending') THEN
+			RAISE EXCEPTION 'No pending configuration exists to encrypt';
+		END IF;
+
+    UPDATE cs_configuration_v1 SET state = 'encrypting' WHERE state = 'pending';
+		RETURN true;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION cs_activate_v1()
+  RETURNS boolean
+AS $$
+	BEGIN
+
+	  IF EXISTS (SELECT FROM cs_configuration_v1 c WHERE c.state = 'encrypting') THEN
+	  	UPDATE cs_configuration_v1 SET state = 'inactive' WHERE state = 'active';
+			UPDATE cs_configuration_v1 SET state = 'active' WHERE state = 'encrypting';
+			RETURN true;
+		ELSE
+			RAISE EXCEPTION 'No encrypting configuration exists to activate';
+		END IF;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION cs_discard_v1()
+  RETURNS boolean
+AS $$
+  BEGIN
+    IF EXISTS (SELECT FROM cs_configuration_v1 c WHERE c.state = 'pending') THEN
+        DELETE FROM cs_configuration_v1 WHERE state = 'pending';
+      RETURN true;
+    ELSE
+      RAISE EXCEPTION 'No pending configuration exists to discard';
+    END IF;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION cs_add_column_v1(table_name text, column_name text)
+  RETURNS jsonb
+AS $$
+  DECLARE
+    key text;
+    _config jsonb;
+  BEGIN
+    -- set the active config
+    SELECT data INTO _config FROM cs_configuration_v1 WHERE state = 'active' OR state = 'pending' ORDER BY state DESC;
+
+    -- set default config
+    SELECT _cs_config_default(_config) INTO _config;
+
+    -- if index exists
+    IF _config #> array['tables', table_name] ?  column_name THEN
+      RAISE EXCEPTION 'Config exists for column: % %', table_name, column_name;
+    END IF;
+
+    SELECT _cs_config_add_table(table_name, _config) INTO _config;
+
+    SELECT _cs_config_add_column(table_name, column_name, _config) INTO _config;
+
+    --  create a new pending record if we don't have one
+    INSERT INTO cs_configuration_v1 (state, data) VALUES ('pending', _config)
+    ON CONFLICT (state)
+      WHERE state = 'pending'
+    DO UPDATE
+      SET data = _config;
+
+    -- exeunt
+    RETURN _config;
+  END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION cs_remove_column_v1(table_name text, column_name text)
+  RETURNS jsonb
+AS $$
+  DECLARE
+    key text;
+    _config jsonb;
+  BEGIN
+     -- set the active config
+    SELECT data INTO _config FROM cs_configuration_v1 WHERE state = 'active' OR state = 'pending' ORDER BY state DESC;
+
+    -- if no config
+    IF _config IS NULL THEN
+      RAISE EXCEPTION 'No active or pending configuration exists';
+    END IF;
+
+    -- if the table doesn't exist
+    IF NOT _config #> array['tables'] ? table_name THEN
+      RAISE EXCEPTION 'No configuration exists for table: %', table_name;
+    END IF;
+
+    -- if the column does not exist
+    IF NOT _config #> array['tables', table_name] ?  column_name THEN
+      RAISE EXCEPTION 'No configuration exists for column: % %', table_name, column_name;
+    END IF;
+
+    --  create a new pending record if we don't have one
+    INSERT INTO cs_configuration_v1 (state, data) VALUES ('pending', _config)
+    ON CONFLICT (state)
+      WHERE state = 'pending'
+    DO NOTHING;
+
+    -- remove the column
+    SELECT _config #- array['tables', table_name, column_name] INTO _config;
+
+    -- if table  is now empty, remove the table
+    IF _config #> array['tables', table_name] = '{}' THEN
+      SELECT _config #- array['tables', table_name] INTO _config;
+    END IF;
+
+    -- if config empty delete
+    -- or update the config
+    IF _config #> array['tables'] = '{}' THEN
+      DELETE FROM cs_configuration_v1 WHERE state = 'pending';
+    ELSE
+      UPDATE cs_configuration_v1 SET data = _config WHERE state = 'pending';
+    END IF;
+
+    -- exeunt
+    RETURN _config;
+
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION cs_refresh_encrypt_config()
+  RETURNS void
+LANGUAGE sql STRICT PARALLEL SAFE
+BEGIN ATOMIC
+  RETURN NULL;
+END;
