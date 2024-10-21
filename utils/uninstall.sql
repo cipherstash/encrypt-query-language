0bf925c737d055e87ead1c08d8fb21a43b6a5f5b
CJ Brewer <cj@cipherstash.com>
2024-10-21 13:08:37 -0600

    Update uninstall.sql

--- utils/uninstall.sql
+++ utils/uninstall.sql
@@ -20,7 +20,7 @@ DROP FUNCTION IF EXISTS cs_activate_v1();
 DROP FUNCTION IF EXISTS cs_discard_v1();
 DROP FUNCTION IF EXISTS cs_refresh_encrypt_config();
 DROP FUNCTION IF EXISTS _cs_config_default(jsonb);
-DROP FUNCTION IF EXISTS _cs_config_match_1_default();
+DROP FUNCTION IF EXISTS _cs_config_match_default();
 DROP FUNCTION IF EXISTS _cs_config_add_table(text, jsonb);
 DROP FUNCTION IF EXISTS _cs_config_add_column(text, text, jsonb);
 DROP FUNCTION IF EXISTS _cs_config_add_cast(text, text, text, jsonb);
@@ -41,6 +41,7 @@ DROP FUNCTION IF EXISTS cs_ore_64_8_v1(jsonb);
 DROP FUNCTION IF EXISTS cs_ore_64_8_v1_v0(jsonb);
 DROP FUNCTION IF EXISTS cs_ore_64_8_v1_v0_0(jsonb);
 DROP FUNCTION IF EXISTS _cs_text_to_ore_64_8_v1_term_v1_0(text) CASCADE;
+DROP FUNCTION IF EXISTS cs_check_encrypted_v1(jsonb);
 DROP FUNCTION IF EXISTS _cs_encrypted_check_kind(jsonb);
 DROP FUNCTION IF EXISTS _cs_config_check_indexes(jsonb);
 DROP FUNCTION IF EXISTS _cs_config_check_cast(jsonb);
@@ -59,7 +60,7 @@ DROP TABLE IF EXISTS cs_configuration_v1;
 -- Drop domains
 DROP DOMAIN IF EXISTS cs_match_index_v1;
 DROP DOMAIN IF EXISTS cs_unique_index_v1;
-DROP DOMAIN IF EXISTS cs_ste_vec_v1;
+DROP DOMAIN IF EXISTS cs_ste_vec_index_v1;
 DROP DOMAIN IF EXISTS cs_encrypted_v1;  -- Note: This domain cannot be dropped if it's in use
 DROP DOMAIN IF EXISTS cs_configuration_data_v1;
 
