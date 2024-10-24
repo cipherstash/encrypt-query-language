50edc31ee425e064d124e0cc8969c01cc000ae2a
Toby Hede <toby@cipherstash.com>
2024-10-24 14:59:32 +1100

    Add ORE install and missing drops

--- sql/dsl-drop.sql
+++ sql/dsl-drop.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS cs_configuration_v1 CASCADE;
+ALTER DOMAIN cs_configuration_data_v1 DROP CONSTRAINT IF EXISTS cs_configuration_data_v1_check;
 ALTER DOMAIN cs_encrypted_v1 DROP CONSTRAINT IF EXISTS cs_encrypted_v1_check;
