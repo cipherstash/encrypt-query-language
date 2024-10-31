fcb560f3b7e631eec747e77557dc616f49bfb85f
Drew Thomas <drew@cipherstash.com>
2024-10-31 16:24:42 +1100

    Add support for `GROUP BY` with `cs_grouped_value_v1`
    This change adds support for decrypting encrypted columns included in the
    `GROUP BY` clause by using a new aggregate function, `cs_grouped_value_v1`.
    
    How it works:
    - An ORE term (either in an ORE or SteVec index) can be used in `GROUP BY`
    - `cs_grouped_value_v1` can be used in the `SELECT` target list to return a
        single source ciphertext for each group
    - the ORE index is used for the equality check needed for grouping
    - `cs_grouped_value_v1` allows for using the source ciphertext in the `SELECT`
       target list (since `SELECT`ed items must either use an aggregrate function or
       appear in the `GROUP BY`)
    - note that including the source ciphertext in the `GROUP BY` statement wouldn't
      work because source ciphertexts are non-deterministic and don't support
      comparison operations (that's what ORE indexes are for)

--- sql/010-core.sql
+++ sql/010-core.sql
@@ -220,3 +220,17 @@ CREATE FUNCTION cs_ore_64_8_v1(col jsonb)
 BEGIN ATOMIC
 	RETURN cs_ore_64_8_v1_v0_0(col);
 END;
+
+DROP FUNCTION IF EXISTS _cs_first_grouped_value(jsonb, jsonb);
+
+CREATE FUNCTION _cs_first_grouped_value(jsonb, jsonb)
+RETURNS jsonb AS $$
+  SELECT COALESCE($1, $2);
+$$ LANGUAGE sql IMMUTABLE;
+
+DROP AGGREGATE IF EXISTS cs_grouped_value_v1(jsonb);
+
+CREATE AGGREGATE cs_grouped_value_v1(jsonb) (
+  SFUNC = _cs_first_grouped_value,
+  STYPE = jsonb
+);
