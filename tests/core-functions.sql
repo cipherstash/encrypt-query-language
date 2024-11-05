13bf286bbb8ae1170efc820e4689024198458652
Toby Hede <toby@cipherstash.com>
2024-11-05 12:52:28 +1100

    Add ore and sv tests

--- tests/core-functions.sql
+++ tests/core-functions.sql
@@ -5,8 +5,44 @@ DO $$
   BEGIN
     ASSERT (SELECT EXISTS (SELECT cs_unique_v1('{"u": "u"}'::jsonb)));
     ASSERT (SELECT EXISTS (SELECT cs_match_v1('{"m": []}'::jsonb)));
+    ASSERT (SELECT EXISTS (SELECT cs_ste_vec_v1('{"sv": [[]]}'::jsonb)));
+    ASSERT (SELECT EXISTS (SELECT cs_ore_64_8_v1('{"o": "()"}'::jsonb)));
+
   END;
 $$ LANGUAGE plpgsql;
+
+DO $$
+  BEGIN
+    -- sanity check
+    PERFORM cs_ore_64_8_v1('{"o": "()"}'::jsonb);
+
+    BEGIN
+      PERFORM cs_ore_64_8_v1('{}'::jsonb);
+      RAISE NOTICE 'Missing index. Function call should have failed.';
+      ASSERT false;
+    EXCEPTION
+      WHEN OTHERS THEN
+        ASSERT true;
+    END;
+  END;
+$$ LANGUAGE plpgsql;
+
+DO $$
+  BEGIN
+    -- sanity check
+    PERFORM cs_ste_vec_v1('{"sv": [[]]}'::jsonb);
+
+    BEGIN
+      PERFORM cs_ste_vec_v1('{}'::jsonb);
+      RAISE NOTICE 'Missing index. Function call should have failed.';
+      ASSERT false;
+    EXCEPTION
+      WHEN OTHERS THEN
+        ASSERT true;
+    END;
+  END;
+$$ LANGUAGE plpgsql;
+
 
 DO $$
   BEGIN
