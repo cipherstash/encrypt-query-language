fff0995333ad3297157a831052ac6234bfb313d3
Yuji Yokoo <yujiyokoo@gmail.com>
2024-11-06 17:51:34 +0900

    Update Python model and types

--- languages/python/jupyter_notebook/cs_types.py
+++ languages/python/jupyter_notebook/cs_types.py
@@ -2,6 +2,7 @@ from psycopg2.extras import RealDictCursor
 from pprint import pprint
 from datetime import datetime
 import json
+from enum import Enum
 
 class CsValue:
     def __init__(self, v, t: str, c: str):
@@ -9,10 +10,10 @@ class CsValue:
         self.table = t
         self.column = c
 
-    def to_db_format(self, query_type=None):
+    def to_db_format(self, query_type = None):
         data = {
             "k": "pt",
-            "p": self.value_in_db_format(query_type),
+            "p": self._value_in_db_format(query_type),
             "i": {
               "t": str(self.table),
               "c": str(self.column)
@@ -24,61 +25,58 @@ class CsValue:
 
     @classmethod
     def from_parsed_json(cls, parsed):
-        return cls.value_from_db_format(parsed["p"])
+        return cls._value_from_db_format(parsed["p"])
 
 class CsInt(CsValue):
-    def value_in_db_format(self, query_type = None):
+    def _value_in_db_format(self, query_type):
         return str(self.value)
 
     @classmethod
-    def value_from_db_format(cls, s: str):
+    def _value_from_db_format(cls, s: str):
         return int(s)
 
 class CsBool(CsValue):
-    def value_in_db_format(self, query_type = None):
+    def _value_in_db_format(self, query_type):
         return str(self.value).lower()
 
     @classmethod
-    def value_from_db_format(cls, s: str):
+    def _value_from_db_format(cls, s: str):
         return s.lower() == 'true'
 
 class CsDate(CsValue):
-    def value_in_db_format(self, query_type = None):
+    def _value_in_db_format(self, query_type):
         return self.value.isoformat()
 
     @classmethod
-    def value_from_db_format(cls, s: str):
+    def _value_from_db_format(cls, s: str):
         return datetime.fromisoformat(s).date()
 
 class CsFloat(CsValue):
-    def value_in_db_format(self, query_type = None):
+    def _value_in_db_format(self, query_type):
         return str(self.value)
 
     @classmethod
-    def value_from_db_format(cls, s: str):
+    def _value_from_db_format(cls, s: str):
         return float(s)
 
 class CsText(CsValue):
-    def value_in_db_format(self, query_type = None):
+    def _value_in_db_format(self, query_type):
         return self.value
 
     @classmethod
-    def value_from_db_format(cls, s: str):
+    def _value_from_db_format(cls, s: str):
         return s
 
 class CsJsonb(CsValue):
-    def value_in_db_format(self, query_type):
+    def _value_in_db_format(self, query_type):
         if query_type == "ejson_path":
             return self.value
         else:
             return json.dumps(self.value)
 
     @classmethod
-    def value_from_db_format(cls, s: str):
+    def _value_from_db_format(cls, s: str):
         return json.loads(s)
-
-def id_map(x):
-    return x
 
 class CsRow:
     column_function_mapping = {
@@ -90,9 +88,15 @@ class CsRow:
         'encrypted_jsonb': CsText.from_parsed_json
     }
 
+    @staticmethod
+    def id_map(x):
+        return x
+
     def __init__(self, row):
         self.row = {}
         for k, v in row.items():
-            self.row[k] = None if v == None else self.column_function_mapping.get(k, id_map)(v)
-
-
+            if v == None:
+                self.row[k] = None
+            else:
+                mapping = self.column_function_mapping.get(k, self.id_map)
+                self.row[k] = mapping(v)
