cfc7a6968ac108faa8b9ccef55fa26dca7d9a8f9
Toby Hede <toby@cipherstash.com>
2025-01-10 12:29:38 +1100

    DB credentials

--- tasks/reset.sh
+++ tasks/reset.sh
@@ -3,7 +3,7 @@
 
 set -euxo pipefail
 
-connection_url=postgresql://${CS_DATABASE__USERNAME:-$USER}:@localhost:$CS_DATABASE__PORT/$CS_DATABASE__NAME
+connection_url=postgresql://${CS_DATABASE__USERNAME:-$USER}@localhost:$CS_DATABASE__PORT/$CS_DATABASE__NAME
 
 # Uninstall
 PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt-uninstall.sql
