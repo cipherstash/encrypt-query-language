ea4080f347c97d60a634fcaa1cba14c9afce082e
Lindsay Holmwood <lindsay@holmwood.id.au>
2025-01-10 12:18:43 +1100

    Remove leading whitespace that was causing shebang to fail

--- tasks/reset.sh
+++ tasks/reset.sh
@@ -1,13 +1,11 @@
-  #!/usr/bin/env bash
-  #MISE description="Clean install of EQL"
+#!/bin/bash
+#MISE description="Clean install of EQL"
 
-  #!/bin/bash
+set -euxo pipefail
 
-  set -euxo pipefail
+connection_url=postgresql://${CS_DATABASE__USERNAME:-$USER}:@localhost:$CS_DATABASE__PORT/$CS_DATABASE__NAME
 
-  connection_url=postgresql://${CS_DATABASE__USERNAME:-$USER}:@localhost:$CS_DATABASE__PORT/$CS_DATABASE__NAME
+# Uninstall
+PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt-uninstall.sql
 
-  # Uninstall
-  PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt-uninstall.sql
-
-  PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt.sql
+PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt.sql
