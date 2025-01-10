fd727bc1a121683ae6982e845f59e86c089d66ba
Lindsay Holmwood <lindsay@holmwood.id.au>
2025-01-10 17:06:08 +1100

    Set the password in the connection string
    To keep it all in one place

--- tasks/reset.sh
+++ tasks/reset.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#MISE description="Clean install of EQL"
+#MISE description="Uninstall and install EQL to local postgres"
 
 set -euxo pipefail
 
-connection_url=postgresql://${CS_DATABASE__USERNAME:-$USER}:@localhost:$CS_DATABASE__PORT/$CS_DATABASE__NAME
+connection_url=postgresql://${CS_DATABASE__USERNAME:-$USER}:${CS_DATABASE__PASSWORD}@localhost:$CS_DATABASE__PORT/$CS_DATABASE__NAME
 
 # Uninstall
-PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt-uninstall.sql
+psql ${connection_url} -f release/cipherstash-encrypt-uninstall.sql
 
-PGPASSWORD=$CS_DATABASE__PASSWORD psql $connection_url -f release/cipherstash-encrypt.sql
+# Install
+psql ${connection_url} -f release/cipherstash-encrypt.sql
