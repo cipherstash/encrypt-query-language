9c91f8c073f1fae6e4529e223e088c0f1fa85e18
Toby Hede <toby@cipherstash.com>
2025-01-30 13:44:25 +1100

    WIP

--- sql/666-drop-operators.sql
+++ sql/666-drop-operators.sql
@@ -1,5 +1,6 @@
-DROP OPERATOR FAMILY IF EXISTS cs_encrypted_ore_64_8_v1_btree_ops_v1 USING btree;
-DROP OPERATOR CLASS IF EXISTS ore_64_8_v1_btree_ops USING btree;
+DROP OPERATOR FAMILY IF EXISTS cs_encrypted_ore_64_8_v1_btree_ops_v1;
+DROP OPERATOR CLASS IF EXISTS cs_encrypted_ore_64_8_v1_btree_ops_v1;
+
 DROP OPERATOR IF EXISTS @> (cs_encrypted_v1, cs_encrypted_v1);
 DROP OPERATOR IF EXISTS @> (cs_encrypted_v1, cs_match_index_v1);
 DROP OPERATOR IF EXISTS @> (cs_match_index_v1, cs_encrypted_v1);
