cd7aa7f41bfe42f7818562d1ab4cd1f6d2781623
Yuji Yokoo <yujiyokoo@gmail.com>
2025-04-02 17:38:31 +0900

    feat: implement cs_min_v1 and cs_max_v1

--- tasks/test.sh
+++ tasks/test.sh
@@ -45,6 +45,7 @@ run_test tests/encryptindex.sql
 run_test tests/operators-eq.sql
 run_test tests/operators-match.sql
 run_test tests/operators-ore.sql
+run_test tests/aggregate-ore.sql
 
 echo
 echo '###############################################'
