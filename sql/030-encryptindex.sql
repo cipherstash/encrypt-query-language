43188bcb832757bf0b2db387dffdcbb17fbac369
Toby Hede <toby@cipherstash.com>
2025-04-11 11:45:54 +1000

    feat(schema) schemify encryptindex

--- sql/030-encryptindex.sql
+++ sql/030-encryptindex.sql
@@ -1,9 +1,9 @@
 -- Return the diff of two configurations
 -- Returns the set of keys in a that have different values to b
 -- The json comparison is on object values held by the key
-DROP FUNCTION IF EXISTS _cs_diff_config_v1(a JSONB, b JSONB);
+DROP FUNCTION IF EXISTS eql_v1.diff_config(a JSONB, b JSONB);
 
-CREATE FUNCTION _cs_diff_config_v1(a JSONB, b JSONB)
+CREATE FUNCTION eql_v1.diff_config(a JSONB, b JSONB)
 	RETURNS TABLE(table_name TEXT, column_name TEXT)
 IMMUTABLE STRICT PARALLEL SAFE
 AS $$
@@ -34,9 +34,9 @@ $$ LANGUAGE plpgsql;
 
 -- Returns the set of columns with pending configuration changes
 -- Compares the columns in pending configuration that do not match the active config
-DROP FUNCTION IF EXISTS cs_select_pending_columns_v1();
+DROP FUNCTION IF EXISTS eql_v1.select_pending_columns();
 
-CREATE FUNCTION cs_select_pending_columns_v1()
+CREATE FUNCTION eql_v1.select_pending_columns()
 	RETURNS TABLE(table_name TEXT, column_name TEXT)
 AS $$
 	DECLARE
@@ -44,14 +44,14 @@ AS $$
 		pending JSONB;
 		config_id BIGINT;
 	BEGIN
-		SELECT data INTO active FROM cs_configuration_v1 WHERE state = 'active';
+		SELECT data INTO active FROM eql_v1_configuration WHERE state = 'active';
 
 		-- set default config
     IF active IS NULL THEN
       active := '{}';
     END IF;
 
-		SELECT id, data INTO config_id, pending FROM cs_configuration_v1 WHERE state = 'pending';
+		SELECT id, data INTO config_id, pending FROM eql_v1_configuration WHERE state = 'pending';
 
 		-- set default config
 		IF config_id IS NULL THEN
@@ -59,7 +59,7 @@ AS $$
 		END IF;
 
 		RETURN QUERY
-		SELECT d.table_name, d.column_name FROM _cs_diff_config_v1(active, pending) as d;
+		SELECT d.table_name, d.column_name FROM eql_v1.diff_config(active, pending) as d;
 	END;
 $$ LANGUAGE plpgsql;
 
@@ -72,9 +72,9 @@ $$ LANGUAGE plpgsql;
 -- On initial encryption from plaintext the target column will be `{column_name}_encrypted `
 -- OR NULL if the column does not exist
 --
-DROP FUNCTION IF EXISTS cs_select_target_columns_v1();
+DROP FUNCTION IF EXISTS eql_v1.select_target_columns();
 
-CREATE FUNCTION cs_select_target_columns_v1()
+CREATE FUNCTION eql_v1.select_target_columns()
 	RETURNS TABLE(table_name TEXT, column_name TEXT, target_column TEXT)
 	STABLE STRICT PARALLEL SAFE
 AS $$
@@ -83,7 +83,7 @@ AS $$
     c.column_name,
     s.column_name as target_column
   FROM
-    cs_select_pending_columns_v1() c
+    eql_v1.select_pending_columns() c
   LEFT JOIN information_schema.columns s ON
     s.table_name = c.table_name AND
     (s.column_name = c.column_name OR s.column_name = c.column_name || '_encrypted') AND
@@ -93,15 +93,15 @@ $$ LANGUAGE sql;
 
 --
 -- Returns true if all pending columns have a target (encrypted) column
-DROP FUNCTION IF EXISTS cs_ready_for_encryption_v1();
+DROP FUNCTION IF EXISTS eql_v1.ready_for_encryption();
 
-CREATE FUNCTION cs_ready_for_encryption_v1()
+CREATE FUNCTION eql_v1.ready_for_encryption()
 	RETURNS BOOLEAN
 	STABLE STRICT PARALLEL SAFE
 AS $$
 	SELECT EXISTS (
 	  SELECT *
-	  FROM cs_select_target_columns_v1() AS c
+	  FROM eql_v1.select_target_columns() AS c
 	  WHERE c.target_column IS NOT NULL);
 $$ LANGUAGE sql;
 
@@ -113,16 +113,16 @@ $$ LANGUAGE sql;
 -- Executes the ALTER TABLE statement
 --   `ALTER TABLE {target_table} ADD COLUMN {column_name}_encrypted cs_encrypted_v1;`
 --
-DROP FUNCTION IF EXISTS cs_create_encrypted_columns_v1();
+DROP FUNCTION IF EXISTS eql_v1.create_encrypted_columns();
 
-CREATE FUNCTION cs_create_encrypted_columns_v1()
+CREATE FUNCTION eql_v1.create_encrypted_columns()
 	RETURNS TABLE(table_name TEXT, column_name TEXT)
 AS $$
 	BEGIN
     FOR table_name, column_name IN
-      SELECT c.table_name, (c.column_name || '_encrypted') FROM cs_select_target_columns_v1() AS c WHERE c.target_column IS NULL
+      SELECT c.table_name, (c.column_name || '_encrypted') FROM eql_v1.select_target_columns() AS c WHERE c.target_column IS NULL
     LOOP
-		  EXECUTE format('ALTER TABLE %I ADD column %I cs_encrypted_v1', table_name, column_name);
+		  EXECUTE format('ALTER TABLE %I ADD column %I eql_v1_encrypted', table_name, column_name);
       RETURN NEXT;
     END LOOP;
 	END;
@@ -138,14 +138,14 @@ $$ LANGUAGE plpgsql;
 --   `ALTER TABLE {target_table} RENAME COLUMN {column_name} TO {column_name}_plaintext;
 --   `ALTER TABLE {target_table} RENAME COLUMN {column_name}_encrypted TO {column_name};`
 --
-DROP FUNCTION IF EXISTS cs_rename_encrypted_columns_v1();
+DROP FUNCTION IF EXISTS eql_v1.rename_encrypted_columns();
 
-CREATE FUNCTION cs_rename_encrypted_columns_v1()
+CREATE FUNCTION eql_v1.rename_encrypted_columns()
 	RETURNS TABLE(table_name TEXT, column_name TEXT, target_column TEXT)
 AS $$
 	BEGIN
     FOR table_name, column_name, target_column IN
-      SELECT * FROM cs_select_target_columns_v1() as c WHERE c.target_column = c.column_name || '_encrypted'
+      SELECT * FROM eql_v1.select_target_columns() as c WHERE c.target_column = c.column_name || '_encrypted'
     LOOP
 		  EXECUTE format('ALTER TABLE %I RENAME %I TO %I;', table_name, column_name, column_name || '_plaintext');
 		  EXECUTE format('ALTER TABLE %I RENAME %I TO %I;', table_name, target_column, column_name);
@@ -155,16 +155,16 @@ AS $$
 $$ LANGUAGE plpgsql;
 
 
-DROP FUNCTION IF EXISTS cs_count_encrypted_with_active_config_v1(table_name TEXT, column_name TEXT);
+DROP FUNCTION IF EXISTS eql_v1.count_encrypted_with_active_config(table_name TEXT, column_name TEXT);
 
-CREATE FUNCTION cs_count_encrypted_with_active_config_v1(table_name TEXT, column_name TEXT)
+CREATE FUNCTION eql_v1.count_encrypted_with_active_config(table_name TEXT, column_name TEXT)
   RETURNS BIGINT
 AS $$
 DECLARE
   result BIGINT;
 BEGIN
 	EXECUTE format(
-        'SELECT COUNT(%I) FROM %s t WHERE %I->>%L = (SELECT id::TEXT FROM cs_configuration_v1 WHERE state = %L)',
+        'SELECT COUNT(%I) FROM %s t WHERE %I->>%L = (SELECT id::TEXT FROM eql_v1_configuration WHERE state = %L)',
         column_name, table_name, column_name, 'v', 'active'
     )
 	INTO result;
