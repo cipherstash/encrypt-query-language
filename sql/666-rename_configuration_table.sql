12e1f63bd8ca6ed483b8e126ff2a4550d7ad2000
Toby Hede <toby@cipherstash.com>
2025-04-11 11:50:42 +1000

    feat(schema) schemify drops

--- sql/666-rename_configuration_table.sql
+++ sql/666-rename_configuration_table.sql
@@ -1,10 +1,10 @@
 -- DANGEROUS
--- DROP TABLE IF EXISTS cs_configuration_v1 CASCADE;
--- ALTER TABLE cs_configuration_v1 RENAME TO cs_configuration_v1_;
+-- DROP TABLE IF EXISTS eql_v1_configuration CASCADE;
+-- ALTER TABLE eql_v1_configuration RENAME TO eql_v1_configuration_;
 
 DO $$
 BEGIN
- EXECUTE format('ALTER TABLE IF EXISTS %I RENAME TO %I_%s', 'cs_configuration_v1','cs_configuration_v1_', to_char(current_date,'YYYYMMDD')::TEXT);
+ EXECUTE format('ALTER TABLE IF EXISTS %I RENAME TO %I_%s', 'eql_v1_configuration','eql_v1_configuration_', to_char(current_date,'YYYYMMDD')::TEXT);
 END
 $$;
 
