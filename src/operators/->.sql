f9dd6ebbcf78aa8ca4aef139ffc28b4595cd79bb
Toby Hede <toby@cipherstash.com>
2025-05-06 14:30:35 +1000

    feat: -> operator with integer for array index

--- src/operators/->.sql
+++ src/operators/->.sql
@@ -46,9 +46,50 @@ AS $$
 $$ LANGUAGE plpgsql;
 
 
+--
+
+
+DROP OPERATOR IF EXISTS -> (eql_v1_encrypted, integer);
+
+DROP FUNCTION IF EXISTS eql_v1."->"(e eql_v1_encrypted, selector integer);
+
+CREATE FUNCTION eql_v1."->"(e eql_v1_encrypted, selector integer)
+  RETURNS eql_v1_encrypted
+  IMMUTABLE STRICT PARALLEL SAFE
+AS $$
+  DECLARE
+    sv eql_v1_encrypted[];
+    found eql_v1_encrypted;
+	BEGIN
+    IF NOT eql_v1.is_ste_vec_array(e) THEN
+      RETURN NULL;
+    END IF;
+
+    sv := eql_v1.ste_vec(e);
+
+    -- PostgreSQL arrays are 1-based
+    -- JSONB arrays are 0-based and so the selector is 0-based
+    FOR idx IN 1..array_length(sv, 1) LOOP
+      if (idx-1) = selector THEN
+        found := sv[idx];
+      END IF;
+    END LOOP;
+
+    RETURN found;
+  END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE OPERATOR ->(
   FUNCTION=eql_v1."->",
   LEFTARG=eql_v1_encrypted,
   RIGHTARG=text
+);
+
+
+CREATE OPERATOR ->(
+  FUNCTION=eql_v1."->",
+  LEFTARG=eql_v1_encrypted,
+  RIGHTARG=integer
 );
 
--- src/operators/->_test.sql
+++ src/operators/->_test.sql
@@ -4,6 +4,7 @@
 SELECT create_table_with_encrypted();
 SELECT seed_encrypted_json();
 
+
 --
 -- The -> operator returns an encrypted matching the selector
 DO $$
@@ -48,6 +49,31 @@ DO $$
         'Fetch ciphertext via selector returns all eql_v1_encrypted',
         'SELECT eql_v1.ciphertext(e->''2517068c0d1f9d4d41d2c666211f785e'') FROM encrypted;',
         3);
+  END;
+$$ LANGUAGE plpgsql;
+
+
+
+--
+-- encrypted returned from -> operator expression called via eql_v1.ciphertext
+--
+DO $$
+  DECLARE
+    result eql_v1_encrypted;
+  BEGIN
+
+    PERFORM truncate_table_with_encrypted();
+    PERFORM seed_encrypted(get_array_ste_vec()::eql_v1_encrypted);
+
+    PERFORM assert_result(
+      '-> operator with integer returns array',
+      'SELECT eql_v1.jsonb_path_query_first(e, ''f510853730e1c3dbd31b86963f029dd5'')->0 as e FROM encrypted');
+
+    PERFORM assert_count(
+         '-> operator with integer returns array',
+        'SELECT eql_v1.jsonb_path_query_first(e, ''f510853730e1c3dbd31b86963f029dd5'')->0 as e FROM encrypted',
+        1);
+
   END;
 $$ LANGUAGE plpgsql;
 
