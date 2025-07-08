b5f061c6c84f63c01fc64fefcc02b1578b1307f7
Toby Hede <toby@cipherstash.com>
2025-07-08 17:21:20 +1000

    fix: drop eql schema on install

--- tasks/uninstall.sql
+++ tasks/uninstall.sql
@@ -1,10 +1,12 @@
 
 DO $$
 BEGIN
- EXECUTE format('ALTER TABLE IF EXISTS %I RENAME TO %I_%s', 'eql_v2_configuration','eql_v2_configuration_', to_char(current_date,'YYYYMMDD')::TEXT);
+  ALTER TABLE IF EXISTS public.eql_v2_configuration DROP CONSTRAINT IF EXISTS eql_v2_configuration_data_check;
+
+  EXECUTE format('ALTER TABLE IF EXISTS %I RENAME TO %I_%s', 'eql_v2_configuration','eql_v2_configuration_', to_char(current_date,'YYYYMMDD')::TEXT);
+
+  RAISE NOTICE 'EQL configuration archived as %_%','eql_v2_configuration_', to_char(current_date,'YYYYMMDD')::TEXT;
 END
 $$;
-
-ALTER TABLE public.eql_v2_configuration DROP CONSTRAINT IF EXISTS eql_v2_configuration_data_check;
 
 DROP SCHEMA IF EXISTS eql_v2 CASCADE;
