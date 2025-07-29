5d55daf3e57d7c579c49e01aa96e72b67b41d9cd
Toby Hede <toby@cipherstash.com>
2025-07-29 20:21:23 +1000

    test: test for handling of null index terms

--- src/operators/compare_test.sql
+++ src/operators/compare_test.sql
@@ -164,3 +164,44 @@ DO $$
     ASSERT eql_v2.compare(c, a) = 1;
   END;
 $$ LANGUAGE plpgsql;
+
+
+--
+-- Compare hmac_256 when record has a `null` index of higher precedence
+-- TEST COVERAGE FOR BUG FIX
+--
+-- ORE Block indexes `ob` are used in compare before hmac_256 indexes.
+-- If the index term is null `{"ob": null}` it should not be used
+-- Comparing two `null` values is evaluated as equality and hilarity ensues
+--
+
+DO $$
+  DECLARE
+   a eql_v2_encrypted;
+   b eql_v2_encrypted;
+   c eql_v2_encrypted;
+  BEGIN
+    -- generate with `hm` index
+    a := create_encrypted_json(1, 'hm');
+    -- append `null` index
+    a := '{"ob": null}'::jsonb || a::jsonb;
+
+    b := create_encrypted_json(2, 'hm');
+    b := '{"ob": null}'::jsonb || b::jsonb;
+
+    c := create_encrypted_json(3, 'hm');
+    c := '{"ob": null}'::jsonb || c::jsonb;
+
+    ASSERT eql_v2.compare(a, a) = 0;
+    ASSERT eql_v2.compare(a, b) = -1;
+    ASSERT eql_v2.compare(a, c) = -1;
+
+    ASSERT eql_v2.compare(b, b) = 0;
+    ASSERT eql_v2.compare(b, a) = 1;
+    ASSERT eql_v2.compare(b, c) = -1;
+
+    ASSERT eql_v2.compare(c, c) = 0;
+    ASSERT eql_v2.compare(c, b) = 1;
+    ASSERT eql_v2.compare(c, a) = 1;
+  END;
+$$ LANGUAGE plpgsql;
