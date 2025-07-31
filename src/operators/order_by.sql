47c71dcb041aff3b90ccdc6df227a32ddd187c34
Toby Hede <toby@cipherstash.com>
2025-07-31 12:50:36 +1000

    fix: add immutable to cast functions

--- src/operators/order_by.sql
+++ src/operators/order_by.sql
@@ -1,7 +1,6 @@
 -- REQUIRE: src/encrypted/types.sql
 -- REQUIRE: src/ore_block_u64_8_256/types.sql
 -- REQUIRE: src/ore_block_u64_8_256/functions.sql
--- REQUIRE: src/ore_block_u64_8_256/operators.sql
 -- REQUIRE: src/ore_cllw_u64_8/types.sql
 -- REQUIRE: src/ore_cllw_u64_8/functions.sql
 
