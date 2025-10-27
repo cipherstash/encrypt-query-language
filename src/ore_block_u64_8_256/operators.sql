96fd99582c9ad79cd62e827a2b521a667bd9918d
Toby Hede <toby@cipherstash.com>
2025-10-27 15:18:33 +1100

    docs(sql): add Doxygen comments to ORE block module (Phase 3 batch 2)
    Documented 27 database objects implementing Order-Revealing Encryption
    for range queries on encrypted data:
    - 2 types (ore_block_u64_8_256_term, ore_block_u64_8_256_v1)
    - 8 functions (make, extract, comparison, equality)
    - 1 comparison function (ore_block_u64_8_256_term_cmp)
    - 2 casts (to/from JSONB)
    - 6 operator functions (=, <>, <, <=, >, >=)
    - 6 operators supporting encrypted range queries
    - 2 operator class definitions (btree, hash)
    
    Note: operators.sql and operator_class.sql marked as DISABLED
    (not included in build due to performance/architectural decisions)
    
    Progress: Phase 3 batch 2 - ORE block cryptographic module complete

--- src/ore_block_u64_8_256/operators.sql
+++ src/ore_block_u64_8_256/operators.sql
@@ -6,6 +6,17 @@
 -- !REQUIRE: src/ore_block_u64_8_256/types.sql
 -- !REQUIRE: src/ore_block_u64_8_256/functions.sql
 
+--! @brief Equality operator for ORE block types
+--! @internal
+--!
+--! Implements the = operator for direct ORE block comparisons.
+--!
+--! @param a eql_v2.ore_block_u64_8_256 Left operand
+--! @param b eql_v2.ore_block_u64_8_256 Right operand
+--! @return Boolean True if ORE blocks are equal
+--!
+--! @note FILE IS DISABLED - Not included in build
+--! @see eql_v2.compare_ore_block_u64_8_256_terms
 CREATE FUNCTION eql_v2.ore_block_u64_8_256_eq(a eql_v2.ore_block_u64_8_256, b eql_v2.ore_block_u64_8_256)
 RETURNS boolean AS $$
   SELECT eql_v2.compare_ore_block_u64_8_256_terms(a, b) = 0
@@ -13,6 +24,17 @@ $$ LANGUAGE SQL;
 
 
 
+--! @brief Not equal operator for ORE block types
+--! @internal
+--!
+--! Implements the <> operator for direct ORE block comparisons.
+--!
+--! @param a eql_v2.ore_block_u64_8_256 Left operand
+--! @param b eql_v2.ore_block_u64_8_256 Right operand
+--! @return Boolean True if ORE blocks are not equal
+--!
+--! @note FILE IS DISABLED - Not included in build
+--! @see eql_v2.compare_ore_block_u64_8_256_terms
 CREATE FUNCTION eql_v2.ore_block_u64_8_256_neq(a eql_v2.ore_block_u64_8_256, b eql_v2.ore_block_u64_8_256)
 RETURNS boolean AS $$
   SELECT eql_v2.compare_ore_block_u64_8_256_terms(a, b) <> 0
@@ -20,6 +42,17 @@ $$ LANGUAGE SQL;
 
 
 
+--! @brief Less than operator for ORE block types
+--! @internal
+--!
+--! Implements the < operator for direct ORE block comparisons.
+--!
+--! @param a eql_v2.ore_block_u64_8_256 Left operand
+--! @param b eql_v2.ore_block_u64_8_256 Right operand
+--! @return Boolean True if left operand is less than right operand
+--!
+--! @note FILE IS DISABLED - Not included in build
+--! @see eql_v2.compare_ore_block_u64_8_256_terms
 CREATE FUNCTION eql_v2.ore_block_u64_8_256_lt(a eql_v2.ore_block_u64_8_256, b eql_v2.ore_block_u64_8_256)
 RETURNS boolean AS $$
   SELECT eql_v2.compare_ore_block_u64_8_256_terms(a, b) = -1
@@ -27,6 +60,17 @@ $$ LANGUAGE SQL;
 
 
 
+--! @brief Less than or equal operator for ORE block types
+--! @internal
+--!
+--! Implements the <= operator for direct ORE block comparisons.
+--!
+--! @param a eql_v2.ore_block_u64_8_256 Left operand
+--! @param b eql_v2.ore_block_u64_8_256 Right operand
+--! @return Boolean True if left operand is less than or equal to right operand
+--!
+--! @note FILE IS DISABLED - Not included in build
+--! @see eql_v2.compare_ore_block_u64_8_256_terms
 CREATE FUNCTION eql_v2.ore_block_u64_8_256_lte(a eql_v2.ore_block_u64_8_256, b eql_v2.ore_block_u64_8_256)
 RETURNS boolean AS $$
   SELECT eql_v2.compare_ore_block_u64_8_256_terms(a, b) != 1
@@ -34,6 +78,17 @@ $$ LANGUAGE SQL;
 
 
 
+--! @brief Greater than operator for ORE block types
+--! @internal
+--!
+--! Implements the > operator for direct ORE block comparisons.
+--!
+--! @param a eql_v2.ore_block_u64_8_256 Left operand
+--! @param b eql_v2.ore_block_u64_8_256 Right operand
+--! @return Boolean True if left operand is greater than right operand
+--!
+--! @note FILE IS DISABLED - Not included in build
+--! @see eql_v2.compare_ore_block_u64_8_256_terms
 CREATE FUNCTION eql_v2.ore_block_u64_8_256_gt(a eql_v2.ore_block_u64_8_256, b eql_v2.ore_block_u64_8_256)
 RETURNS boolean AS $$
   SELECT eql_v2.compare_ore_block_u64_8_256_terms(a, b) = 1
@@ -41,6 +96,17 @@ $$ LANGUAGE SQL;
 
 
 
+--! @brief Greater than or equal operator for ORE block types
+--! @internal
+--!
+--! Implements the >= operator for direct ORE block comparisons.
+--!
+--! @param a eql_v2.ore_block_u64_8_256 Left operand
+--! @param b eql_v2.ore_block_u64_8_256 Right operand
+--! @return Boolean True if left operand is greater than or equal to right operand
+--!
+--! @note FILE IS DISABLED - Not included in build
+--! @see eql_v2.compare_ore_block_u64_8_256_terms
 CREATE FUNCTION eql_v2.ore_block_u64_8_256_gte(a eql_v2.ore_block_u64_8_256, b eql_v2.ore_block_u64_8_256)
 RETURNS boolean AS $$
   SELECT eql_v2.compare_ore_block_u64_8_256_terms(a, b) != -1
@@ -48,6 +114,8 @@ $$ LANGUAGE SQL;
 
 
 
+--! @brief = operator for ORE block types
+--! @note FILE IS DISABLED - Not included in build
 CREATE OPERATOR = (
   FUNCTION=eql_v2.ore_block_u64_8_256_eq,
   LEFTARG=eql_v2.ore_block_u64_8_256,
@@ -61,6 +129,8 @@ CREATE OPERATOR = (
 
 
 
+--! @brief <> operator for ORE block types
+--! @note FILE IS DISABLED - Not included in build
 CREATE OPERATOR <> (
   FUNCTION=eql_v2.ore_block_u64_8_256_neq,
   LEFTARG=eql_v2.ore_block_u64_8_256,
@@ -73,6 +143,8 @@ CREATE OPERATOR <> (
 );
 
 
+--! @brief > operator for ORE block types
+--! @note FILE IS DISABLED - Not included in build
 CREATE OPERATOR > (
   FUNCTION=eql_v2.ore_block_u64_8_256_gt,
   LEFTARG=eql_v2.ore_block_u64_8_256,
@@ -85,6 +157,8 @@ CREATE OPERATOR > (
 
 
 
+--! @brief < operator for ORE block types
+--! @note FILE IS DISABLED - Not included in build
 CREATE OPERATOR < (
   FUNCTION=eql_v2.ore_block_u64_8_256_lt,
   LEFTARG=eql_v2.ore_block_u64_8_256,
@@ -97,6 +171,8 @@ CREATE OPERATOR < (
 
 
 
+--! @brief <= operator for ORE block types
+--! @note FILE IS DISABLED - Not included in build
 CREATE OPERATOR <= (
   FUNCTION=eql_v2.ore_block_u64_8_256_lte,
   LEFTARG=eql_v2.ore_block_u64_8_256,
@@ -109,6 +185,8 @@ CREATE OPERATOR <= (
 
 
 
+--! @brief >= operator for ORE block types
+--! @note FILE IS DISABLED - Not included in build
 CREATE OPERATOR >= (
   FUNCTION=eql_v2.ore_block_u64_8_256_gte,
   LEFTARG=eql_v2.ore_block_u64_8_256,
