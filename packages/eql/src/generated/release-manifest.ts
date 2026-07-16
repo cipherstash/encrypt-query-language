e7790ab6429b92e6d3cd61d6364c6eadb9b039c4
Toby Hede <toby@cipherstash.com>
2026-07-16 14:57:22 +1000

    chore(release): eql 3.0.1-alpha.0

--- packages/eql/src/generated/release-manifest.ts
+++ packages/eql/src/generated/release-manifest.ts
@@ -1,6 +1,6 @@
 export const releaseManifest = {
-  eqlVersion: '3.0.0',
+  eqlVersion: '3.0.1-alpha.0',
   schemaVersion: 3,
-  installSqlSha256: '6da5ed462b02f53a0636d783880533496eecfb52218d718791085376bc44c6a0',
+  installSqlSha256: '8e3321fb74e3bef2881cdbe6dc0b94742c7192360b9e94742177794f9476c597',
   uninstallSqlSha256: 'b1b5131b8175c5d04da9ada108d25c81c5772b15fad79a6c419ebb32d18c60a9',
 } as const
