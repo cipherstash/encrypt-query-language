045dcc83f12a4bdd06c26f941274dffc5058bcdd
Dan Draper <dan@cipherstash.com>
2026-07-20 16:11:33 +1000

    Regenerate 3.0.2 release assets

--- packages/eql/src/generated/release-manifest.ts
+++ packages/eql/src/generated/release-manifest.ts
@@ -1,6 +1,6 @@
 export const releaseManifest = {
   eqlVersion: '3.0.2',
   schemaVersion: 3,
-  installSqlSha256: '58a160c2e1b2508ae6ffd84e8f8e5285d690f20ea5cc9d65edcea8467ada3c40',
+  installSqlSha256: '05860ae47b3760cbba9842b22ddf89cf3f03aa49c33b6386f736c271784094b1',
   uninstallSqlSha256: 'b1b5131b8175c5d04da9ada108d25c81c5772b15fad79a6c419ebb32d18c60a9',
 } as const
